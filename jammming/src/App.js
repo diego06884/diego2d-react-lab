e2c923338c47c19dd3a3ffdf20ebdc0d4c2e8712
Diego Vergara <diego06884@hotmail.com>
2019-06-04 16:40:50 -0500

    Add Progress

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -1,29 +1,35 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import Spotify from './util/Spotify';
 import React, {useState} from 'react';
+
 const SearchResultsList = [
   {
+    id: 'id1',
     title: 'Tiny Dancer',
     artist: 'Elton John',
     album: 'Madman Across The Water'
   },
   {
+    id: 'id2',
     title: 'Tiny Dancer',
     artist: 'Elton John',
     album: 'Madman Across The Water'
   },
   {
+    id: 'id3',
     title: 'Tiny Dancer',
     artist: 'Elton John',
     album: 'Madman Across The Water'
   },
   {
+    id: 'id4',
     title: 'Tiny Dancer',
     artist: 'Elton John',
     album: 'Madman Across The Water'
   }
 ]
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   const [searchQuery, setSearchQuery] = useState('');
   
   const changeInput = (e) => {
@@ -31,23 +37,32 @@ const SearchBar = () => {
     setSearchQuery(inputValue);
   }
 
+  const onSearchClick = (e) => {
+    props.doSearchClick(searchQuery);
+    setSearchQuery(''); 
+  }
+
   return (
     <div className="SearchBar">
           <input value={searchQuery} onChange={changeInput} placeholder="Enter A Song Title" />
-          <a>SEARCH</a>
+          <button className='btn btn-primary' type='button' onClick={onSearchClick}>SEARCH</button>
     </div>
     )
 };
 
 const Track = (props) => {
   const {title, artist, album} = props;
+  const onTrackActionClick = (e) => {
+    e.preventDefault();
+    
+  }
   return (
-    <div className="Track">
+    <div  className="Track">
       <div className="Track-information">
         <h3>{title}</h3>
         <p>{artist} | {album}</p>
       </div>
-      <a className="Track-action">+</a>
+      <a className="Track-action" onClick="onTrackActionClick">+</a>
     </div>
   )
 }
@@ -55,23 +70,33 @@ const TrackList = (props) => {
   const {tracks} = props;
   return (
     <div className="TrackList">  
-      {tracks.map((track, index) => <Track key={index} title={track.title} artist={track.artist} album={track.title}/>)}
+      {tracks.map((track, index) => <Track key={track.id} title={track.title} artist={track.artist} album={track.title}/>)}
     </div>
   )
     
 }
 function App() {
+
+  const [searchResults, setSearchResults] = useState([]);
+  const [newPlaylist, setNewPlaylist] = useState([]);
+  
+  const doSearchClick = async (searchQuery) => {
+    const newSearchResults = await Spotify.doSearchQuery(searchQuery);
+    setSearchResults(newSearchResults);
+  }
+  
+  
   return (
     <div className="App">
-        <SearchBar/>
+        <SearchBar doSearchClick={doSearchClick}/>
         <div className="App-playlist">
           <div className="SearchResults">
             <h2>Results</h2>
-            <TrackList tracks={SearchResultsList}/>
+            <TrackList tracks={searchResults}/>
           </div>
           <div className="Playlist">
           <input value='New Playlist' />
-              <TrackList tracks={SearchResultsList}/>
+              <TrackList tracks={newPlaylist}/>
             <a className="Playlist-save">SAVE TO SPOTIFY</a>
           </div>
         </div>
